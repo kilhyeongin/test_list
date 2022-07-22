6b4a75f956962447a27ddf93768994772bf2613e
kilhyeongin <yaro2kil@naver.com>
2022-07-22 12:15:01 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [listNmae, reListName] = useState([])
+  let [addList, setAddList] = useState('')
+
+  let [listdate, reListdate] = useState([])
+  let [addNal, setAddNal] = useState('')
+
+  let [good, regood] = useState([])
+  let [modal, setModal] = useState(false)
+  let [titleNo, setTitleNo] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <div>방명록 - 길형인</div>
+        <div>저에게 하고싶은 말 남겨주세요.</div>
       </header>
-    </div>
+      {
+        modal == true ? <Modal listNmae={listNmae} titleNo={titleNo}></Modal> : null
+      }
+      {
+      listNmae.map((a,i)=>{
+        return(
+            <div className="list" key={i}>
+              <p className='listname' onClick={()=>{
+                setModal(!modal)
+                setTitleNo(i)
+                }}>{a}</p>
+
+              <p className='good'><span onClick={()=>{
+                let copy = [...good]
+                copy[i] = copy[i]+1
+                regood(copy)
+                }}>😊</span> {good[i]} </p>
+
+              <p className='date'>{listdate[i]}</p>
+
+              <button className='but2' onClick={()=>{
+                let copy = [...listNmae]
+                copy.splice(i, 1)
+                reListName(copy)
+                let copy2 = [...good]
+                copy2.splice(i, 1)
+                regood(copy2)
+              }}>삭제</button>
+
+            </div>
+        )
+      })
+      }
+
+      <div className='input'>
+        <p>제목<input className='ip' type='text' onChange={(e)=>{
+          setAddList(e.target.value)
+          }} /></p>
+
+        <p>내용 <textarea className='ip2' onChange={(e)=>{
+          setAddNal(e.target.value)
+          }} /></p>
+
+        <button className='but' onClick={()=>{
+          let copy = [...listNmae]
+          copy.unshift(addList)
+          reListName(copy)
+
+          let copy2 = [...good]
+          copy2.unshift(0)
+          regood(copy2)
+
+          let copy3 = [...listdate]
+          copy3.unshift(addNal)
+          reListdate(copy3)
+
+
+        }}>전송</button>
+      </div>
+    </>
   );
 }
-
+function Modal(props){
+  return(
+    <>
+        <div className='modal'>
+          <h4>{props.listNmae[props.titleNo]}</h4>
+          <p>상세내용</p>
+          <p>날자</p>
+        </div>
+    </>
+  )
+}
 export default App;
